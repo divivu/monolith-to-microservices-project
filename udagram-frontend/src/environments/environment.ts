5d4ec4173750609afc8b6eddf97a32be2183481a
div <div.hung@gmail.com>
2023-06-27 12:43:57 +0700

    replace with External IP

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae82b260f412d41229be375aa1a66386-1231808093.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
