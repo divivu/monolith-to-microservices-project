da2ff570a0ebee9b98d17170014f3fb920404640
div <div.hung@gmail.com>
2023-06-29 18:35:32 +0700

    update environtment.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a88101a81ad4e4982a27b8b816f18321-1265141354.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
